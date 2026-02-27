ab0b90df01d9aba8988a6ade2d1a51dcdfd6b95c
raiz <128097369+razingash@users.noreply.github.com>
2026-02-27 19:03:53 +0100

    0.0.3
    Added openapi

--- supra/contacts/urls.py
+++ supra/contacts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('api/contacts/', ContactsApiView.as_view(), name='contacts'), # get | post
-    path('api/contacts/<int:contact_id>/', ContactApiView.as_view(), name='contact'), # get | put | delete
+    path('contacts/', ContactsApiView.as_view(), name='contacts'), # get | post
+    path('contacts/<int:contact_id>/', ContactApiView.as_view(), name='contact'), # get | put | delete
 ]
 
--- supra/supra/settings.py
+++ supra/supra/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contacts.apps.ContactsConfig',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -88,6 +89,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
